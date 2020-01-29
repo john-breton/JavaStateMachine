df2d55b4860a9d57ccfa7a06d219954e51066ee0
John Breton <55368611+john-breton@users.noreply.github.com>
2020-01-29 13:08:02 -0500

    Update Documentation

--- src/main/java/elevatorSubsystem/Elevator.java
+++ src/main/java/elevatorSubsystem/Elevator.java
@@ -1,18 +1,37 @@
 package elevatorSubsystem;
 
+import java.util.*;
+
 /**
- * 
- * @author
+ * The Elevator class represents nodes within the elevatorSubsystem package.
+ * Each Elevator serves as one node, continually polling the scheduler for pending work.
+ * For Iteration 1, once an Elevator receives work from the scheduler, it takes the
+ * information and immediately tries to sends it back to the scheduler.
+ *
+ * @author John Breton
  * @version Iteration 1 - February 1st, 2020
  */
-public class Elevator {
+public class Elevator implements Runnable {
+	private Deque workQueue;
 
 	/**
-	 * First Iteration.
-	 * 
-	 * @param args The command line arguments.
+	 * Construct a new Elevator
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public Elevator() {
+		workQueue = new ArrayDeque<>();
 	}
+
+	private void checkScheduler() {
+
+	}
+
+	private void passToScheduler(ArrayList data) {
+
+	}
+
+	@Override
+	public void run() {
+		// TODO: Complete run behaviour for the class.
+	}
+
 }
