ab9487216f51adac82ea980aad121c1a4c83458a
Justice <justiceodia@cmail.carleton.ca>
2020-02-01 19:02:24 -0500

    Floor Class Testing

--- src/test/java/floorSubsystem/FloorTest.java
+++ src/test/java/floorSubsystem/FloorTest.java
@@ -5,21 +5,57 @@ package floorSubsystem;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import scheduler.Scheduler;
 
 /**
  * @author osayimwense
  *
  */
 class FloorTest {
-
+	
+	Floor validFloor;
+	Floor invalidFloor;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		validFloor = new Floor(new Scheduler());
+		invalidFloor = new Floor(null);
+
+	}
+	
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void restoreStreams() {
+	    System.setOut(originalOut);
+	}
+	
+	@Test
+	void exceptionTesting() {
+		
+		assertThrows(NullPointerException.class, () -> invalidFloor.run(),
+				"Expected run() to throw since scheduler is null");
 		
 	}
 
@@ -32,7 +68,12 @@ class FloorTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+			assertNotSame(validFloor.fetchRequests(),0);
+			assertNotSame(invalidFloor.fetchRequests(),0);
+			validFloor.displayAllRequests();
+		    assertNotNull(outContent.toString());
+
 	}
+	
 
 }
